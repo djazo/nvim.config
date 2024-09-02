1c81efcceb9b6a93348f3811a0e0adb1cbcefbc5
Arto Kitula <arto.kitula@gmail.com>
2024-09-02 21:12:01 +0300

    move from neogit to lazygit

---
+++ lua/user/lazygit.lua
@@ -0,0 +1,18 @@
+local M = {
+  "kdheepak/lazygit.nvim",
+  cmd = {
+    "LazyGit",
+    "LazyGitConfig",
+    "LazyGitCurrentFile",
+    "LazyGitFilter",
+    "LazyGitFilterCurrentFile",
+  },
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+  },
+  keys = {
+    {"<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit"}
+}
+}
+
+return M
