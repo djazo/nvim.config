6ab2304c08a9c409eb1ac92f7aaeb8e02d530012
Arto Kitula <arto.kitula@gmail.com>
2025-04-06 23:22:27 +0300

    add which-key binds for telescope

--- lua/user/telescope.lua
+++ lua/user/telescope.lua
@@ -8,6 +8,19 @@ local M = {
 }
 
 function M.config()
+  local wk = require "which-key"
+  wk.add {
+    { "<leader>bb", "<cmd>Telescope buffers previewer=false<cr>", desc = "Find" },
+    { "<leader>fb", "<cmd>Telescope git_branches<cr>", desc = "Checkout branch" },
+    { "<leader>fc", "<cmd>Telescope colorscheme<cr>", desc = "Colorscheme" },
+    { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find files" },
+    { "<leader>fp", "<cmd>lua require('telescope').extensions.projects.projects()<cr>", desc = "Projects" },
+    { "<leader>ft", "<cmd>Telescope live_grep<cr>", desc = "Find Text" },
+    { "<leader>fh", "<cmd>Telescope help_tags<cr>", desc = "Help" },
+    { "<leader>fl", "<cmd>Telescope resume<cr>", desc = "Last Search" },
+    { "<leader>fr", "<cmd>Telescope oldfiles<cr>", desc = "Recent File" },  
+  }
+
   local icons = require "user.icons"
   local actions = require "telescope.actions"
 
