a7af438b21d63cd6c075ff59131acc961867c898
Arto Kitula <arto.kitula@gmail.com>
2025-04-07 04:38:42 +0300

    tab navigation with shift

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -14,3 +14,7 @@ keymap("n","<C-h>","<C-w>h",opts)
 keymap("n","<C-l>","<C-w>l",opts)
 keymap("n","<C-j>","<C-w>j",opts)
 keymap("n","<C-k>","<C-w>k",opts)
+
+-- tab navigation
+keymap("n","<s-h>", function() vim.cmd('tabp') end, opts)
+keymap("n","<s-l>", function() vim.cmd('tabn') end, opts)
