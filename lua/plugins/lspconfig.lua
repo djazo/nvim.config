25750acf88a5e52590965919f6912c8466b202e5
Arto Kitula <arto.kitula@gmail.com>
2025-06-10 00:44:15 +0300

    lspconfig start

---
+++ lua/plugins/lspconfig.lua
@@ -0,0 +1,13 @@
+local M = {
+  'neovim/nvim-lspconfig',
+  config = function()
+    vim.lsp.enable('clangd',{ cmd = '/opt/homebrew/opt/llvm/bin/clangd' })
+    vim.lsp.enable('lua_ls')
+    vim.lsp.enable('bashls')
+    vim.lsp.enable('cmake')
+    vim.lsp.enable('dockerls')
+    vim.lsp.enable('dotls')
+  end
+}
+
+return M
