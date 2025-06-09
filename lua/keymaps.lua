0abc17481a529d95caf4695db9b95c5ce13e4282
Arto Kitula <arto.kitula@gmail.com>
2025-06-09 23:21:43 +0300

    remove space also from visual

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,7 +1,7 @@
 local keymap = vim.keymap.set
 local opts = { noremap = true, silent = true }
 
-keymap("n", "<Space>", "", opts)
+keymap({"n","v"}, "<Space>", "", opts)
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
@@ -10,3 +10,14 @@ keymap("n", "<C-h>", "<C-w>h", opts)
 keymap("n", "<C-j>", "<C-w>j", opts)
 keymap("n", "<C-k>", "<C-w>k", opts)
 keymap("n", "<C-l>", "<C-w>l", opts)
+
+-- don't put deleted chars into buf
+keymap("n", "x", '"_x', opts)
+
+-- sizing
+keymap("n","<Up>", ":resize -2<CR>", opts)
+keymap("n","<Down>", ":resize +2<CR", opts)
+keymap("n","<Left>", ":vertical resize -2<CR>", opts)
+keymap("n","<Right>", "vertical resize +2<CR>", opts)
+
+
