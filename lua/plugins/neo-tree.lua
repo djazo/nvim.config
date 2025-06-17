0f1d78e1d9042551f0626e9b7a6b1cb676b7bb87
Arto Kitula <arto.kitula@gmail.com>
2025-06-17 20:20:00 +0300

    add neotree config

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -11,6 +11,22 @@ local M = {
   },
   config = function()
     vim.keymap.set("n", "<leader>e", ":Neotree toggle position=left<CR>", { noremap = true, silent = true })
+    require('neo-tree').setup({
+    filesystem = {
+      filtered_items = {
+        visible = true,
+        hide_dotfiles = false,
+        hide_gitignored = false,
+        hide_by_name = {
+          ".DS_Store",
+          "thumbs.db",
+          ".git",
+          ".hg",
+          ".svn",
+        },
+      },
+    }
+    })
   end
 }
 
