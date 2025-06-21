7d9c2e5e8f0c97cb98aed6eb6748cd4d0a536638
Arto Kitula <arto.kitula@gmail.com>
2025-06-21 12:44:38 +0300

    have keymaps on one file

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -2,8 +2,6 @@ local keymap = vim.keymap.set
 local opts = { noremap = true, silent = true }
 
 keymap({"n","v"}, "<Space>", "", opts)
-vim.g.mapleader = " "
-vim.g.maplocalleader = " "
 
 -- Better window navigation
 keymap("n", "<C-h>", "<C-w>h", opts)
@@ -19,5 +17,30 @@ keymap("n","<Up>", ":resize -2<CR>", opts)
 keymap("n","<Down>", ":resize +2<CR>", opts)
 keymap("n","<Left>", ":vertical resize -2<CR>", opts)
 keymap("n","<Right>", ":vertical resize +2<CR>", opts)
+
+-- telescope keymaps
+local builtin = require 'telescope.builtin'
+keymap('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
+keymap('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
+keymap('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
+keymap('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
+keymap('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
+keymap('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
+keymap('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
+keymap('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
+keymap('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
+keymap('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
+
+-- lsp keymaps
+
+keymap('n', '<leader>lf', function()
+  vim.lsp.buf.format({ async = true })
+end , { desc = '[L]SP [F]ormat' })
+keymap('n', '<leader>li', vim.lsp.buf.implementation, { desc = '[L]SP [I]mplementation' })
+keymap('n', '<leader>ld', vim.lsp.buf.definition, { desc = '[L]SP [D]efinition' })
+keymap('n', '<leader>lr', vim.lsp.buf.references, { desc = '[L]SP [R]eferences' })
+keymap('n', '<leader>lh', vim.lsp.buf.hover, { desc = '[L]SP [H]over' })
+keymap('n', '<leader>la', vim.lsp.buf.code_action, { desc = '[L]SP [A]ction' })
+keymap('n', '<leader>ln', vim.lsp.buf.rename, { desc = '[L]SP re[N]ame' })
 
 
