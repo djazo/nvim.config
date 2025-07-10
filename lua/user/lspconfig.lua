88f07ae19853f880f107518f28a710a6cfd09586
Arto Kitula <arto.kitula@gmail.com>
2025-07-11 01:42:02 +0300

    lspconfig add inlay hints

--- lua/user/lspconfig.lua
+++ lua/user/lspconfig.lua
@@ -1,5 +1,8 @@
 local M = {
   'neovim/nvim-lspconfig',
+  opts = {
+    inlay_hints = { enabled = true },
+  },
   event = { 'BufReadPre', 'BufNewFile' },
 }
 
@@ -7,15 +10,19 @@ function M.config()
   local lspconfig = require('lspconfig')
   local icons = require('user.icons')
 
-  vim.lsp.enable('bashls')
-  vim.lsp.enable('clangd', {
-    cmd = '/opt/homebrew/opt/llvm/bin/clangd'
+  vim.lsp.config('clangd', {
+    cmd = {
+      '/opt/homebrew/opt/llvm/bin/clangd',
+      '--clang-tidy',
+      '--background-index',
+      '--header-insertion=iwyu',
+      '--completion-style=detailed',
+      '--function-arg-placeholders',
+      '--style=file',
+    },
   })
-  vim.lsp.enable('cmake')
-  vim.lsp.enable('dockerls')
-  vim.lsp.enable('dotls')
-  vim.lsp.enable('lua_ls')
-  vim.lsp.enable('texlab', {
+
+  vim.lsp.config('texlab', {
     settings = {
       texlab = {
         forwardSearch = {
@@ -25,30 +32,15 @@ function M.config()
         }
       }
     }
+  })
 
-  })
-  local capabilities = require('cmp_nvim_lsp').default_capabilities()
-  require('lspconfig')['clangd'].setup({
-    capabilities = capabilities
-  })
-  require('lspconfig')['bashls'].setup({
-    capabilities = capabilities
-  })
-  require('lspconfig')['cmake'].setup({
-    capabilities = capabilities
-  })
-  require('lspconfig')['dockerls'].setup({
-    capabilities = capabilities
-  })
-  require('lspconfig')['dotls'].setup({
-    capabilities = capabilities
-  })
-  require('lspconfig')['lua_ls'].setup({
-    capabilities = capabilities
-  })
-  require('lspconfig')['texlab'].setup({
-    capabilities = capabilities
-  })
+  vim.lsp.enable('bashls')
+  vim.lsp.enable('clangd')
+  vim.lsp.enable('cmake')
+  vim.lsp.enable('dockerls')
+  vim.lsp.enable('dotls')
+  vim.lsp.enable('lua_ls')
+  vim.lsp.enable('texlab')
 
   vim.diagnostic.config({
     signs = {
