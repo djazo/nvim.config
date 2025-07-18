9d9414b2c9d6a4934bb0df6cbc083c11d108fea9
Arto Kitula <arto.kitula@gmail.com>
2025-07-18 16:00:06 +0300

    use asciidoc cmd preview rather than js thingies

---
+++ lua/user/asciidoc.lua
@@ -0,0 +1,10 @@
+local M = {
+  'tigion/nvim-asciidoc-preview',
+  opts = {
+    server = {
+      converter = 'cmd',
+    },
+  }
+}
+
+return M
--- lua/user/livepreview.lua
+++
@@ -1,5 +0,0 @@
-local M = {
-  'brianhuster/live-preview.nvim',
-}
-
-return M
