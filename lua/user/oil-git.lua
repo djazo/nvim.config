0fa806e4777e994560e26ff7ce67ec9313c88bf3
Arto Kitula <arto.kitula@gmail.com>
2026-02-23 18:09:43 +0200

    use oil-git, neater

---
+++ lua/user/oil-git.lua
@@ -0,0 +1,5 @@
+local M = {
+  'benomahony/oil-git.nvim',
+}
+
+return M
--- lua/user/oil.lua
+++ lua/user/oil.lua
@@ -5,9 +5,6 @@ local M = {
   },
   config = function()
     require('oil').setup({
-      win_options = {
-        signcolumn = "yes:3",
-      },
     })
   end
 }
