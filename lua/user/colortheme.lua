f705fe29d70e7b8721862e6b5180c4bd872baeb4
Arto Kitula <arto.kitula@gmail.com>
2026-02-23 14:28:46 +0200

    colorize

---
+++ lua/user/colortheme.lua
@@ -0,0 +1,16 @@
+local M = {
+  'shaunsingh/nord.nvim',
+  lazy = false,
+  priority = 10000,
+  config = function()
+    vim.g.nord_contrast = true
+    vim.g.nord_borders = false
+    vim.g.nord_disable_background = true
+    vim.g.nord_italic = false
+    vim.g.nord_uniform_diff_background = true
+    vim.g.nord_bold = false
+    require('nord').set()
+  end
+}
+
+return M
